3435088392b09e81a1b98423eaaab73dd29f46f1
Rushikesh Udhoji <32307213+rushiudh@users.noreply.github.com>
2026-08-31 17:29:55 +0530

    Add: sql-create-table - submission-1

---
+++ SQL/sql-create-table/submission-1.sql
@@ -0,0 +1,12 @@
+
+
+create table videos(name text, age integer, join_date date);
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
